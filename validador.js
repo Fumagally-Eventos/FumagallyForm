f8247bfb0018496afff3566602f3268de665327a
Fumagally-Eventos <fumagallyeventos@gmail.com>
2025-05-09 20:52:04 -0300

    prr prr

--- validador.js
+++ validador.js
@@ -1,4 +1,5 @@
 (function () {
+  document.getElementById("prrprrpatapim").remove();
   console.log("O arquivo JavaScript foi carregado com sucesso!");
 
   // Exemplo de alerta para verificar a execução
