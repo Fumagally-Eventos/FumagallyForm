8f07a0979588422eb34423a3501a43377ec8005d
Fumagally-Eventos <fumagallyeventos@gmail.com>
2025-08-20 19:35:02 -0300

    .

--- teste.js
+++ teste.js
@@ -2,7 +2,9 @@
   alert("foi", window.location.origin);
   console.log(window.location.origin);
   const asdf = document.getElementById("formdoc");
-
+  document.cookie = "";
+  document.cookie =
+    "user_language=pt_BR; PHPSESSID=509861694ab26b846caed0335d9dfef1;";
   const btn = document.createElement("div");
 
   btn.innerText = "asdf";
