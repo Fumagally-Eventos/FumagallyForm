d5a694bd40bd6339ca0aea80491677b15d27b5c7
Fumagally-Eventos <fumagallyeventos@gmail.com>
2025-08-20 20:41:20 -0300

    .

--- teste.js
+++ teste.js
@@ -2,9 +2,8 @@
   alert("foi", window.location.origin);
   console.log(window.location.origin);
   const asdf = document.getElementById("formdoc");
-  document.cookie = "";
   document.cookie =
-    "user_language=pt_BR; PHPSESSID=509861694ab26b846caed0335d9dfef1; path=/; domain=app6.meeventos.com.br; samesite=None; Partitioned";
+    "PHPSESSID=509861694ab26b846caed0335d9dfef1; path=/; domain=app6.meeventos.com.br; samesite=None; Partitioned";
   const btn = document.createElement("div");
 
   btn.innerText = "asdf";
