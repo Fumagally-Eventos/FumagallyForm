3f76f2ace9229edbe2bc40dd7dd111ca727456a5
Fumagally-Eventos <fumagallyeventos@gmail.com>
2025-08-20 18:53:24 -0300

    .

--- teste.js
+++ teste.js
@@ -1,4 +1,14 @@
 (function () {
   alert("foi", window.location.origin);
   console.log(window.location.origin);
+  const asdf = document.getElementById("formdoc");
+
+  const btn = document.createElement("div");
+
+  btn.innerText = "asdf";
+  btn.onclick = () => {
+    console.log(window.location.origin);
+  };
+
+  asdf.appendChild(btn);
 })();
