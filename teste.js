decbd0a44da21f13e6e82454fe85426dd0edad96
Fumagally-Eventos <fumagallyeventos@gmail.com>
2025-08-20 22:15:07 -0300

    .

--- teste.js
+++ teste.js
@@ -1,31 +1,30 @@
 (function () {
+  const EXPECTED_PARENT_ORIGIN = "http://localhost:3000";
+  async function fetchData(email) {
+    return await fetch(
+      "https://app6.meeventos.com.br/fumagallyeventos/index.php?p=visualizar&pagina=consultacadastrocliente",
+      {
+        credentials: "include",
+        headers: {
+          Accept: "*/*",
+          "Accept-Language": "en-US,en;q=0.5",
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          "X-Requested-With": "XMLHttpRequest",
+          "Sec-Fetch-Dest": "empty",
+          "Sec-Fetch-Mode": "cors",
+          "Sec-Fetch-Site": "same-origin",
+          Priority: "u=0",
+        },
+        referrer:
+          "https://app6.meeventos.com.br/fumagallyeventos/index.php?p=orcamento&acao=novo",
+        body: `email=${email}`,
+        method: "POST",
+        mode: "cors",
+      }
+    );
+  }
+
   function initBridge() {
-    const EXPECTED_PARENT_ORIGIN = "http://localhost:3000";
-
-    async function fetchData(email) {
-      return await fetch(
-        "https://app6.meeventos.com.br/fumagallyeventos/index.php?p=visualizar&pagina=consultacadastrocliente",
-        {
-          credentials: "include",
-          headers: {
-            Accept: "*/*",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "X-Requested-With": "XMLHttpRequest",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin",
-            Priority: "u=0",
-          },
-          referrer:
-            "https://app6.meeventos.com.br/fumagallyeventos/index.php?p=orcamento&acao=novo",
-          body: `email=${email}`,
-          method: "POST",
-          mode: "cors",
-        }
-      );
-    }
-
     window.addEventListener("message", (e) => {
       console.log(e, "asdfoi");
       if (e.origin !== EXPECTED_PARENT_ORIGIN) return; // segurança básica
@@ -55,26 +54,30 @@
   btn.onclick = () => {
     console.log(document.cookie);
     async function fetchData() {
-      await fetch(
-        "https://app6.meeventos.com.br/fumagallyeventos/index.php?p=visualizar&pagina=consultacadastrocliente",
-        {
-          credentials: "include",
-          headers: {
-            Accept: "*/*",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "X-Requested-With": "XMLHttpRequest",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin",
-            Priority: "u=0",
-          },
-          referrer:
-            "https://app6.meeventos.com.br/fumagallyeventos/index.php?p=orcamento&acao=novo",
-          body: "email=eternopupilo%40gmail.com",
-          method: "POST",
-          mode: "cors",
-        }
+      // await fetch(
+      //   "https://app6.meeventos.com.br/fumagallyeventos/index.php?p=visualizar&pagina=consultacadastrocliente",
+      //   {
+      //     credentials: "include",
+      //     headers: {
+      //       Accept: "*/*",
+      //       "Accept-Language": "en-US,en;q=0.5",
+      //       "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+      //       "X-Requested-With": "XMLHttpRequest",
+      //       "Sec-Fetch-Dest": "empty",
+      //       "Sec-Fetch-Mode": "cors",
+      //       "Sec-Fetch-Site": "same-origin",
+      //       Priority: "u=0",
+      //     },
+      //     referrer:
+      //       "https://app6.meeventos.com.br/fumagallyeventos/index.php?p=orcamento&acao=novo",
+      //     body: "email=eternopupilo%40gmail.com",
+      //     method: "POST",
+      //     mode: "cors",
+      //   }
+      // );
+      window.parent.postMessage(
+        { type: "SET_EMAIL_ACK", data: "taltal" },
+        "http://localhost:3000"
       );
     }
     fetchData();
