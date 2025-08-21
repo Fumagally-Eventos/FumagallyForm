4897391440434ae1f23a9fe1112e17a35af7707c
Fumagally-Eventos <fumagallyeventos@gmail.com>
2025-08-20 21:37:32 -0300

    .

--- teste.js
+++ teste.js
@@ -1,5 +1,84 @@
 (function () {
-  alert("foi", window.location.origin);
+  function initBridge() {
+    const EXPECTED_PARENT_ORIGIN = "http://localhost:3000";
+
+    async function fetchData(email) {
+      return await fetch(
+        "https://app6.meeventos.com.br/fumagallyeventos/index.php?p=visualizar&pagina=consultacadastrocliente",
+        {
+          credentials: "include",
+          headers: {
+            Accept: "*/*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "X-Requested-With": "XMLHttpRequest",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+            Priority: "u=0",
+          },
+          referrer:
+            "https://app6.meeventos.com.br/fumagallyeventos/index.php?p=orcamento&acao=novo",
+          body: `email=${email}`,
+          method: "POST",
+          mode: "cors",
+        }
+      );
+    }
+
+    window.addEventListener("message", (e) => {
+      if (e.origin !== EXPECTED_PARENT_ORIGIN) return; // segurança básica
+      const msg = e.data || {};
+      if (msg.type !== "SET_EMAIL" || typeof msg.email !== "string") return;
+      const resp = fetchData(msg.email);
+      e.source?.postMessage({ type: "SET_EMAIL_ACK", resp: resp }, e.origin);
+    });
+
+    // try {
+    //   window.parent.postMessage(
+    //     { type: "IFRAME_READY" },
+    //     EXPECTED_PARENT_ORIGIN
+    //   );
+    // } catch {}
+  }
+  initBridge();
+
+  React.useEffect(() => {
+    const iframeRef = React.useRef < HTMLIFrameElement > null;
+    const IFRAME_ORIGIN = "https://app6.meeventos.com.br";
+    function onMessage(e) {
+      const iframeWin = iframeRef.current?.contentWindow;
+      if (!iframeWin) return;
+
+      // Garante que a msg veio do SEU iframe e da origem esperada
+      if (e.source !== iframeWin) return;
+      if (e.origin !== IFRAME_ORIGIN) return;
+
+      const msg = e.data || {};
+      switch (msg.type) {
+        case "IFRAME_READY":
+          console.log("Iframe pronto");
+          // se quiser reenviar o e-mail quando o iframe se anunciar:
+          // iframeWin.postMessage({ type: "SET_EMAIL", email: "foo@bar.com" }, IFRAME_ORIGIN);
+          break;
+
+        case "SET_EMAIL_ACK":
+          console.log("Iframe aceitou o e-mail:", msg);
+          break;
+
+        case "SET_EMAIL_NACK":
+          console.error("Iframe recusou o e-mail:", msg);
+          break;
+
+        default:
+        // ignore
+      }
+    }
+
+    window.addEventListener("message", onMessage);
+    return () => window.removeEventListener("message", onMessage);
+  }, []);
+
   console.log(window.location.origin);
   const asdf = document.getElementById("formdoc");
   document.cookie =
